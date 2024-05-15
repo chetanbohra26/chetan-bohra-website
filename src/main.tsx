da0a97facafaab1e557576f0031139d06edbd0cb
Chetan Bohra <chetanbohra26@gmail.com>
2024-05-15 19:15:35 +0530

    Deployment fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { TooltipProvider } from './components/ui/tooltip.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<TooltipProvider>
-			<App />
-		</TooltipProvider>
+		<App />
 	</React.StrictMode>
 );
