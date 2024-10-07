9748784396f82c9bf33a54d6a7e9d9d7ccc65bc7
Chetan Bohra <chetanbohra26@gmail.com>
2024-10-07 23:04:12 +0530

    Added tooltip to nav icons

--- src/components/views/Navbar.tsx
+++ src/components/views/Navbar.tsx
@@ -1,10 +1,9 @@
+import { LightbulbIcon } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
-// import { Label } from '@/components/ui/label';
 import linkedinIconImg from '@/assets/linkedin-in.svg';
 import githubIconImg from '@/assets/github.svg';
 import mailIconImg from '@/assets/envelope-regular.svg';
-import { LightbulbIcon } from 'lucide-react';
 
 const Navbar = () => {
 	return (
@@ -24,42 +23,62 @@ const Navbar = () => {
 								<p>Have an idea?</p>
 							</TooltipContent>
 						</Tooltip>
+
+						<div className="flex">
+							<Tooltip delayDuration={200}>
+								<TooltipTrigger asChild>
+									<a
+										href="https://www.linkedin.com/in/chetanbohra26"
+										target="_blank"
+										rel="noreferrer"
+									>
+										<Avatar className="h-8 w-8 p-1.5 mr-2 hover:scale-125 transition ease-in-out">
+											<AvatarImage src={linkedinIconImg} />
+											<AvatarFallback>Linkedin</AvatarFallback>
+										</Avatar>
+									</a>
+
+								</TooltipTrigger>
+								<TooltipContent align='center'>
+									<p>LinkedIn</p>
+								</TooltipContent>
+							</Tooltip>
+							<Tooltip delayDuration={200}>
+								<TooltipTrigger asChild>
+									<a
+										href="https://github.com/chetanbohra26"
+										target="_blank"
+										rel="noreferrer"
+									>
+										<Avatar className="h-8 w-8 p-1.5 mr-2 hover:scale-125 transition ease-in-out">
+											<AvatarImage src={githubIconImg} />
+											<AvatarFallback>Github</AvatarFallback>
+										</Avatar>
+									</a>
+								</TooltipTrigger>
+								<TooltipContent align='center'>
+									<p>Github</p>
+								</TooltipContent>
+							</Tooltip>
+							<Tooltip delayDuration={200}>
+								<TooltipTrigger asChild>
+									<a
+										href="mailto:chetanbohra26@gmail.com"
+										target="_blank"
+										rel="noreferrer"
+									>
+										<Avatar className="h-8 w-8 p-1.5 mr-4 hover:scale-125 transition ease-in-out">
+											<AvatarImage src={mailIconImg} />
+											<AvatarFallback>Mail</AvatarFallback>
+										</Avatar>
+									</a>
+								</TooltipTrigger>
+								<TooltipContent align='center'>
+									<p>Mail me</p>
+								</TooltipContent>
+							</Tooltip>
+						</div>
 					</TooltipProvider>
-					{/* <Label className="text-center text-lg">
-						Hi, I am Chetan Bohra
-					</Label> */}
-					<div className="flex">
-						<a
-							href="https://www.linkedin.com/in/chetanbohra26"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<Avatar className="h-8 w-8 p-1.5 mr-2 hover:scale-125 transition ease-in-out">
-								<AvatarImage src={linkedinIconImg} />
-								<AvatarFallback>Linkedin</AvatarFallback>
-							</Avatar>
-						</a>
-						<a
-							href="https://github.com/chetanbohra26"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<Avatar className="h-8 w-8 p-1.5 mr-2 hover:scale-125 transition ease-in-out">
-								<AvatarImage src={githubIconImg} />
-								<AvatarFallback>Github</AvatarFallback>
-							</Avatar>
-						</a>
-						<a
-							href="mailto:chetanbohra26@gmail.com"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<Avatar className="h-8 w-8 p-1.5 mr-4 hover:scale-125 transition ease-in-out">
-								<AvatarImage src={mailIconImg} />
-								<AvatarFallback>Mail</AvatarFallback>
-							</Avatar>
-						</a>
-					</div>
 				</div>
 			</div>
 		</nav>
